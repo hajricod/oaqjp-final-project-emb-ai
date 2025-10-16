f13ea87053c60a276d7947622644e883d394691c
ahmed <hajricod@gmail.com>
2025-10-16 07:32:05 -0400

    updates

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, request, json
+"""Flask app for detecting emotions from text input."""
+
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
-    return render_template('index.html')
+    """Render the home page."""
+    return render_template("index.html")
 
 @app.route("/emotionDetector")
 def emotion_detect():
-
-    text_to_analyze = request.args.get('textToAnalyze')
-
+    """Handle emotion detection requests."""
+    text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
-    if response["dominant_emotion"] != None:
+    if response["dominant_emotion"] is not None:
         return response
-    else:
-        return " Invalid text! Please try again!"
+
+    return "Invalid text! Please try again!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)
